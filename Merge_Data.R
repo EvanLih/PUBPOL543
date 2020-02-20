298bf4be43eb269e5081b09a7e095e9c2963a7d3
Evan Lih <evanlih@uw.edu>
2020-02-20 10:34:11 -0800

    dwad

--- Merge_Data.R
+++ Merge_Data.R
@@ -1,11 +1,15 @@
 final_data <- read.csv("Final Data - Sheet1.csv")
 
+library(ggplot)
+library(magrittr)
+library(tidyr)
+
 data <- table(final_data$Continent, final_data$Income.Group)
 
 data_mgCol <- prop.table(data,margin = 2) 
 
 data_mgCol %<>% as.data.frame() %>%
-  rename("Country"= Var1) %>%
+  rename("Continent"= Var1) %>%
   rename("incomeStatus" = Var2)
 
 ggplot(data_mgCol, aes(Country, Freq)) +
